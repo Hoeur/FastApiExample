601b3c3fd69296985ac1fcc2e3f73032504b0d6e
hoeurlihour <hourofficial359@gmail.com>
2025-04-20 22:57:13 +0700

    fastApi example

---
+++ main.py
@@ -0,0 +1,35 @@
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class User(BaseModel):
+    name: str
+    age: int
+    position: str
+
+users = [{"name":"Hour","age":"23","position":"developer"},{"name":"Nich","age":"21","position":"teacher"}]
+
+@app.get("/")
+async def read_root():
+    return {"Hello", "World"}
+
+@app.get("/users")
+async def get_users():
+    return users
+
+@app.get("/users/{user_id}")
+async def get_users_detail(user_id: int, q: Union[str, None] = None):
+    return {"user_id": user_id, "q": q}    
+
+@app.put("/users/{user_id}")
+async def update_user(user_id: int, users: User):
+    return {"user_name": users.name, "user_id": user_id}
+
+@app.get("/say-hi")
+async def say_hi(name: str | None = None):
+    if name is not None:
+        return f"Hello: {name}"
+    else:
+        return "Hello World"
